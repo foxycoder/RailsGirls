73c22d12ceae4885b7715e19e6806a01f58416ff
Wouter de Vos <wouter@springest.com>
2014-10-26 15:56:26 +0100

    Added the PostDecorator.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.published_and_owned_by(current_user)
+    @posts = PostDecorator.decorate_collection(@posts)
   end
 
   # GET /posts/1
@@ -96,7 +97,7 @@ class PostsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_post
-    @post = Post.published_and_owned_by(current_user).friendly.find(params[:id])
+    @post = Post.published_and_owned_by(current_user).friendly.find(params[:id]).decorate
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
---
+++ app/decorators/post_decorator.rb
@@ -0,0 +1,7 @@
+class PostDecorator < Draper::Decorator
+  delegate_all
+
+  def content
+    object.content.html_safe
+  end
+end
